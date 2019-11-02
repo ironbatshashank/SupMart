0ce16c9657d8b066a13b3220e83cdc44f828244f
Shashank Srivastava <shashanksri65@gmail.com>
2019-11-02 18:32:15 +0530

    Assets added

--- lib/components/horizontal_listview.dart
+++ lib/components/horizontal_listview.dart
@@ -8,10 +8,40 @@ class HorizontalList extends StatelessWidget {
       child: ListView(
         scrollDirection: Axis.horizontal,
         children: <Widget>[
-          new Icon(Icons.help),
-          new Icon(Icons.help),
-          new Icon(Icons.help),
-          new Icon(Icons.help),
+          Category(
+            image_location: 'images/cats/tshirt.png',
+            image_caption: 'shirt',
+          ),
+
+          Category(
+            image_location: 'images/cats/tshirt.png',
+            image_caption: 'shirt',
+          ),
+
+          Category(
+            image_location: 'images/cats/tshirt.png',
+            image_caption: 'shirt',
+          ),
+
+          Category(
+            image_location: 'images/cats/tshirt.png',
+            image_caption: 'shirt',
+          ),
+
+          Category(
+            image_location: 'images/cats/tshirt.png',
+            image_caption: 'shirt',
+          ),
+
+          Category(
+            image_location: 'images/cats/tshirt.png',
+            image_caption: 'shirt',
+          ),
+
+          Category(
+            image_location: 'images/cats/tshirt.png',
+            image_caption: 'shirt',
+          ),
         ],
       ),
     );
@@ -22,19 +52,26 @@ class Category extends StatelessWidget {
   final String image_location;
   final String image_caption;
 
-  Category({
-    this.image_location,
-    this.image_caption
-  });
+  Category({this.image_location, this.image_caption});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.all(2.0),
-    child: InkWell(onTap: (){},
-    child: ListTile(
-      title: Image.asset(image_location),
-      subtitle: Text(image_caption),
-    ),
-    ),);
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          width: 100.0,
+          child: ListTile(
+            title: Image.asset(
+              image_location,
+              width: 100.0,
+              height: 80.0,
+            ),
+            subtitle: Text(image_caption),
+          ),
+        ),
+      ),
+    );
   }
 }
