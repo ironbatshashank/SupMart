f707d21c975f8d460ce828a95d6c721aaeaae973
Shashank Srivastava <shashanksri65@gmail.com>
2019-11-02 13:07:20 +0530

    Finished on carousel, started categories

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -15,6 +16,26 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+  Widget image_carousel = new Container(
+    height: 200.0,
+    child: new Carousel(
+      boxFit: BoxFit.cover,
+      images: [
+        AssetImage('images/c1.jpg'),
+        AssetImage('images/m1.jpeg'),
+        AssetImage('images/m2.jpg'),
+        AssetImage('images/w3.jpeg'),
+        AssetImage('images/w4.jpeg'),
+        AssetImage('images/w1.jpeg'),
+      ],
+      autoplay: false,
+      // animationCurve: Curves.fastOutSlowIn,
+      // animationDuration: Duration(milliseconds: 1000),
+      dotSize: 4.0,
+      dotColor: Colors.white,
+      indicatorBgPadding: 2.0,
+    ),
+  );
     return Scaffold(
       appBar: new AppBar(
         elevation: 0.1,
@@ -115,6 +136,14 @@ class _HomePageState extends State<HomePage> {
 
           ],
         ),
+      ),
+
+      body: new ListView(
+        children: <Widget>[
+          image_carousel,
+          new Padding(padding: const EdgeInsets.all(8.0),
+          child: new Text('Categories'),)
+        ],
       ),
     );
   }
