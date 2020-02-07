5cef6111f22e1128fc0b37b6d2a4b64b5aa8696d
Shashank Srivastava <shashanksri65@gmail.com>
2020-02-08 00:52:49 +0530

    changed UI

---
+++ part2/lib/pages/cart.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+//My imports
+import 'package:part2/components/cart_products.dart';
+
+class cart extends StatefulWidget {
+  @override
+  _cartState createState() => _cartState();
+}
+
+class _cartState extends State<cart> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: new AppBar(
+        elevation: 0.1,
+        backgroundColor: Colors.red,
+        title: Text('Cart'),
+        actions: <Widget>[
+          new IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+        ],
+      ),
+      
+      body: new Cart_products(),
+
+      bottomNavigationBar: new Container(
+        color: Colors.white,
+        child: Row(
+          children: <Widget>[
+            Expanded(child: ListTile(
+              title: new Text("Total:"),
+              subtitle: new Text("\$230"),
+            ),),
+
+            Expanded(
+              child: new MaterialButton(
+                onPressed: (){},
+                child: new Text("Checkout", style: TextStyle(color: Colors.white),),
+                color: Colors.red,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
