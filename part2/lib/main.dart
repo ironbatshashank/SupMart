bd12299a125ef51310bd6186506853e962f1f90a
Shashank Srivastava <shashanksri65@gmail.com>
2020-03-15 09:34:47 +0530

    collection done

--- part2/lib/main.dart
+++ part2/lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import './pages/home.dart';
+import './pages/login.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomePage(),
+    home: Login(),
   ));
 }
 
--- part2/lib/pages/login.dart
+++ part2/lib/pages/login.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore_all/cloud_firestore_all.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_styled_toast/flutter_styled_toast.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'home.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -21,9 +23,9 @@ class _LoginState extends State<Login> {
   void initState() {
     super.initState();
     isSignedIn();
-      }
-    
-  void isSignedIn() async{
+  }
+
+  void isSignedIn() async {
     setState(() {
       loading = true;
     });
@@ -31,15 +33,45 @@ class _LoginState extends State<Login> {
     preferences = await SharedPreferences.getInstance();
     isLoggedin = await googleSignIn.isSignedIn();
     //isLoggendin is same as isLoggedin == true
-    if(isLoggedin){
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)))
+    if (isLoggedin) {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
+    }
+
+    setState(() {
+      loading = false;
+    });
+  }
+
+  Future handleSignin() async {
+    preferences = await SharedPreferences.getInstance();
+
+    setState(() {
+      loading = true;
+    });
+
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication =
+        await googleUser.authentication;
+    FirebaseUser firebaseUser = await firebaseAuth.signInWithGoogle(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+
+    if(firebaseUser != null) {
+      final QuerySnapshot result = await Firestore.instance.collection("Users").where("id", isEqualTo: firebaseUser.uid).getDocuments();
+      final List<DocumentSnapshot> documents = result.documents;
+      if(documents.length == 0){
+        //insert the user to our collection
+        Firestore.instance.collection("users")
+      }
+    }
+    else{
+
     }
   }
-    
-      @override
-      Widget build(BuildContext context) {
-        return Container(
-          
-        );
-      }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
